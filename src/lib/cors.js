2472d57ff844df465ceb06c650d34c25be32ae20
polarparsnip <>
2023-12-08 11:22:31 +0000

    update cors

--- src/lib/cors.js
+++ src/lib/cors.js
@@ -4,7 +4,8 @@ export function cors(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    '*'
+    // 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   next();
 }
